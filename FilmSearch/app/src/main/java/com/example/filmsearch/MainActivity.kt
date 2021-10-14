fabf13e7b62664dd7bbe605f2407c67e27102e8e
skripkalisa <skripkalisa.msk@ya.ru>
2021-10-14 18:26:13 +0300

    Mod24 version is done

--- FilmSearch/app/src/main/java/com/example/filmsearch/MainActivity.kt
+++ FilmSearch/app/src/main/java/com/example/filmsearch/MainActivity.kt
@@ -1,16 +1,72 @@
 package com.example.filmsearch
 
+import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+    private val filmsDataBase = listOf(
+        Film(
+            "The Shawshank Redemption",
+            R.drawable.shawshank,
+            "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.god_father,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        ),
+        Film(
+            "The Dark Knight",
+            R.drawable.dark_knight,
+            "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+        ),
+        Film(
+            "Pulp Fiction",
+            R.drawable.pulp,
+            "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+        ),
+        Film(
+            "Inception",
+            R.drawable.inception,
+            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O."
+        ),
+        Film(
+            "Hamilton",
+            R.drawable.hamilton,
+            "The real life of one of America's foremost founding fathers and first Secretary of the Treasury, Alexander Hamilton. Captured live on Broadway from the Richard Rodgers Theater with the original Broadway cast."
+        ),
+        Film(
+            "Gisaengchung",
+            R.drawable.parasite,
+            "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan."
+        ),
+        Film(
+            "Interstellar",
+            R.drawable.interstellar,
+            "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival."
+        ),
+        Film(
+            "Joker",
+            R.drawable.joker,
+            "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker."
+        ),
+        Film(
+            "1917",
+            R.drawable.seventeen,
+            "April 6th, 1917. As a regiment assembles to wage war deep in enemy territory, two soldiers are assigned to race against time and deliver a message that will stop 1,600 men from walking straight into a deadly trap."
+        )
+
+    )
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,10 +77,40 @@ class MainActivity : AppCompatActivity() {
 
         navBarClicks()
 
+        //находим наш RV
+        val mainRecycler = findViewById<RecyclerView>(R.id.main_recycler)
+        mainRecycler.apply {
+            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
+            //оставим его пока пустым, он нам понадобится во второй части задания
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+                        //Создаем бандл и кладем туда объект с данными фильма
+                        val bundle = Bundle()
+                        //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
+                        //передаваемый объект
+                        bundle.putParcelable("film", film)
+                        //Запускаем наше активити
+                        val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+                        //Прикрепляем бандл к интенту
+                        intent.putExtras(bundle)
+                        //Запускаем активити через интент
+                        startActivity(intent)
+                    }
+                })
+            //Присваиваем адаптер
+            adapter = filmsAdapter
+            //Присвоим layout manager
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            //Применяем декоратор для отступов
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+//Кладем нашу БД в RV
+        filmsAdapter.addItems(filmsDataBase)
 
 
     }
-
 
 
     private fun navBarClicks() {
