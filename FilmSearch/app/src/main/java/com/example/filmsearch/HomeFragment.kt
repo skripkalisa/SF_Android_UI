259fd599f11e56205dd448d9bdedddb31cd7c5bb
skripkalisa <skripkalisa.msk@ya.ru>
2021-11-09 01:53:37 +0300

    Mod28 is done

--- FilmSearch/app/src/main/java/com/example/filmsearch/HomeFragment.kt
+++ FilmSearch/app/src/main/java/com/example/filmsearch/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.example.filmsearch
 
 import android.os.Bundle
+import android.transition.Scene
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.transition.TransitionSet
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -80,6 +86,27 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val homeFragmentRoot = view.findViewById<ConstraintLayout>(R.id.home_fragment_root)
+        val scene = Scene.getSceneForLayout(
+            homeFragmentRoot,
+            R.layout.merge_home_screen_content,
+            requireContext()
+        )
+
+        //Создаем анимацию выезда поля поиска сверху
+        val searchSlide = Slide(Gravity.TOP).addTarget(R.id.search_view)
+//Создаем анимацию выезда RV снизу
+        val recyclerSlide = Slide(Gravity.BOTTOM).addTarget(R.id.main_recycler)
+//Создаем экземпляр TransitionSet, который объединит все наши анимации
+        val customTransition = TransitionSet().apply {
+            //Устанавливаем время, за которое будет проходить анимация
+            duration = 500
+            //Добавляем сами анимации
+            addTransition(recyclerSlide)
+            addTransition(searchSlide)
+        }
+//Также запускаем через TransitionManager, но вторым параметром передаем нашу кастомную анимацию
+        TransitionManager.go(scene, customTransition)
         val searchView = view.findViewById<SearchView>(R.id.search_view)
 
         searchView.setOnClickListener {
@@ -87,11 +114,12 @@ class HomeFragment : Fragment() {
         }
 
         //Подключаем слушателя изменений введенного текста в поиска
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
+
             //Этот метод отрабатывает на каждое изменения текста
             override fun onQueryTextChange(newText: String): Boolean {
                 //Если ввод пуст то вставляем в адаптер всю БД
@@ -102,7 +130,8 @@ class HomeFragment : Fragment() {
                 //Фильтруем список на поискк подходящих сочетаний
                 val result = filmsDataBase.filter {
                     //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
-                    it.title.lowercase(Locale.getDefault()).contains(newText.lowercase(Locale.getDefault()))
+                    it.title.lowercase(Locale.getDefault())
+                        .contains(newText.lowercase(Locale.getDefault()))
                 }
                 //Добавляем в адаптер
                 filmsAdapter.addItems(result)
@@ -110,7 +139,7 @@ class HomeFragment : Fragment() {
             }
         })
 
-        val mainRecycler = view.findViewById<RecyclerView>(R.id.main_recycler_view)
+        val mainRecycler = view.findViewById<RecyclerView>(R.id.main_recycler)
         //находим наш RV
         mainRecycler.apply {
             filmsAdapter =
